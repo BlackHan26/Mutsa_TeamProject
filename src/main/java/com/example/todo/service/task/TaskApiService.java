c78d823cb7c941043adb3335dfe048baa8274e3c
BlackHan26 <128695414+BlackHan26@users.noreply.github.com>
2023-11-23 08:23:50 +0900

    feat: add TaskValidationUtils * TaskValidationUtils에 인증 메서드들 모음 * TaskService 메서드 분리 * 주석 처리

--- src/main/java/com/example/todo/service/task/TaskApiService.java
+++ src/main/java/com/example/todo/service/task/TaskApiService.java
@@ -3,13 +3,9 @@ package com.example.todo.service.task;
 import com.example.todo.domain.entity.MemberEntity;
 import com.example.todo.domain.entity.TeamEntity;
 import com.example.todo.domain.entity.TaskApiEntity;
-import com.example.todo.domain.entity.UsersSubscriptionEntity;
-import com.example.todo.domain.entity.enums.SubscriptionStatus;
 import com.example.todo.domain.entity.user.User;
 import com.example.todo.domain.repository.MemberRepository;
 import com.example.todo.domain.repository.TaskApiRepository;
-import com.example.todo.domain.repository.TeamReposiotry;
-import com.example.todo.domain.repository.UsersSubscriptionRepository;
 import com.example.todo.domain.repository.user.UserRepository;
 import com.example.todo.dto.NotificationDto;
 import com.example.todo.dto.ResponseDto;
@@ -38,77 +34,39 @@ import static com.example.todo.service.team.TeamService.FREE_TEAM_PARTICIPANT_NU
 @RequiredArgsConstructor
 public class TaskApiService {
     private final TaskApiRepository taskApiRepository;
-    private final TeamReposiotry teamRepository;
     private final UserRepository userRepository;
     private final MemberRepository memberRepository;
-    private final UsersSubscriptionRepository usersSubscriptionRepository;
     private final NotificationService notificationService;
+    private final TaskValidationUtils taskValidationUtils;
 
-    //회원인지 확인
-    public User getUserById(Long userId) {
-        return userRepository.findById(userId)
-                .orElseThrow(() -> new TodoAppException(ErrorCode.NOT_FOUND_USER));
-    }
-
-    //조직이 존재하는지 확인하는 메서드
-    public TeamEntity getTeamById(Long teamId) {
-        return teamRepository.findById(teamId)
-                .orElseThrow(() -> new TodoAppException(ErrorCode.NOT_FOUND_TEAM));
-    }
-
-    //업무 존재하는지 확인하는 메서드
-    public TaskApiEntity getTaskById(Long taskId) {
-        return taskApiRepository.findById(taskId)
-                .orElseThrow(() -> new TodoAppException(ErrorCode.NOT_FOUND_TASK));
-    }
-
-    //멤버인지 확인하는 메서드
-    public void isMemberOfTeam(Long userId, Long teamId) {
-        TeamEntity teamEntity = getTeamById(teamId);
-        if (teamEntity.getMembers().stream().noneMatch(member -> member.getUser().getId().equals(userId))) {
-            throw new TodoAppException(ErrorCode.NOT_MATCH_MEMBERID);
-        }
-    }
-    public void isAvailableFunction(TeamEntity teamEntity) {
-        if (teamEntity.getParticipantNumMax() > FREE_TEAM_PARTICIPANT_NUM) {
-            UsersSubscriptionEntity usersSubscription = usersSubscriptionRepository.findByUsersAndSubscriptionStatus(teamEntity.getManager(), SubscriptionStatus.ACTIVE)
-                    .orElseThrow(() -> new TodoAppException(ErrorCode.NOT_AVAILABLE_FUNCTION));
-            if (usersSubscription.getSubscription().getMaxMember() < teamEntity.getParticipantNumMax())
-                throw new TodoAppException(ErrorCode.NOT_AVAILABLE_FUNCTION);
-        }
-    }
-    public void validateTaskAndUser(Long userId, Long teamId, TaskApiEntity taskApiEntity) {
-        // 대상 업무가 대상 팀의 업무가 맞는지
-        if (!teamId.equals(taskApiEntity.getTeam().getId())) {
-            throw new TodoAppException(ErrorCode.NOT_MATCH_TEAM_AND_TASK);
-        }
-
-        // 팀 관리자 or 업무 담당자
-        if (!taskApiEntity.getUserId().equals(userId) && !taskApiEntity.getWorkerId().equals(userId)) {
-            throw new TodoAppException(ErrorCode.NOT_MATCH_USERID);
-        }
-    }
-
-
-    //업무 등록
+    //업무 등록하는 메서드
     public ResponseDto createTask(Long userId, Long teamId, TaskCreateDto taskCreateDto) {
         log.info("TaskApiService createTask1");
         //팀 존재 확인
-        TeamEntity teamEntity = getTeamById(teamId);
+        TeamEntity teamEntity = taskValidationUtils.getTeamById(teamId);
         //by안채연,기능을 사용할 수 있는지 확인
-        isAvailableFunction(teamEntity);
+        taskValidationUtils.isAvailableFunction(teamEntity);
         // 사용자가 해당 팀의 멤버인지 확인
-        isMemberOfTeam(userId, teamId);
-        // 담당자 존재 확인
+        taskValidationUtils.isMemberOfTeam(userId, teamId);
+        // 담당자가 있는 유저인지 확인
         log.info("{}", taskCreateDto.getWorker());
         User worker = userRepository.findByUsername(taskCreateDto.getWorker())
                 .orElseThrow(() -> new TodoAppException(ErrorCode.NOT_FOUND_USER));
 
-        // 담당자가 특정 팀의 멤버인지 확인
-        Long workerUserId = worker.getId();
-        isMemberOfTeam(workerUserId, teamId);
+        // 담당자가 팀의 멤버인지 확인
+        taskValidationUtils.isMemberOfTeam(worker.getId(), teamId);
+        //담당자를 존재를 확인하고 꺼내온다.
+        MemberEntity workerMember = memberRepository.findByTeamAndUser(teamEntity, worker)
+                .orElseThrow(() -> new TodoAppException(ErrorCode.NOT_FOUND_MEMBER)
+                );
+        TaskApiEntity taskApiEntity = createTaskEntity(userId, teamEntity, taskCreateDto, workerMember);
+        taskApiEntity = taskApiRepository.save(taskApiEntity);
 
-        MemberEntity workerMember = memberRepository.findByTeamAndUser(teamEntity, worker).orElseThrow(() -> new TodoAppException(ErrorCode.NOT_FOUND_MEMBER));
+        return new ResponseDto("업무가 등록되었습니다.");
+    }
+
+    //새로운 업무 엔터티를 생성하여 반환하는 메서드
+    private TaskApiEntity createTaskEntity(Long userId, TeamEntity teamEntity, TaskCreateDto taskCreateDto, MemberEntity workerMember) {
         TaskApiEntity taskApiEntity = new TaskApiEntity();
         taskApiEntity.setUserId(userId);
         taskApiEntity.setTeam(teamEntity);
@@ -117,18 +75,26 @@ public class TaskApiService {
         taskApiEntity.setStartDate(taskCreateDto.getStartDate());
         taskApiEntity.setDueDate(taskCreateDto.getDueDate());
         taskApiEntity.setMember(workerMember);
-        //업무 상태 설정
-        setTaskStatus(taskApiEntity, taskCreateDto.getStartDate(), taskCreateDto.getDueDate());
-        taskApiEntity = taskApiRepository.save(taskApiEntity);
-        return new ResponseDto("업무가 등록되었습니다.");
+
+        //현재날짜에 맞춰서 업무 상태를 저장
+        LocalDate currentDate = LocalDate.now();
+        taskApiEntity.setStatus("진행중");
+        //설정 날짜가 미래라면 진행 예정
+        if (taskCreateDto.getStartDate().isAfter(currentDate)) {
+            taskApiEntity.setStatus("진행예정");
+            //설정 날짜가 과거라면 완료
+        } else if (taskCreateDto.getDueDate().isBefore(currentDate)) {
+            taskApiEntity.setStatus("완료");
+        }
+        return taskApiEntity;
     }
 
-    //업무 상세 조회
+    //업무 상세 조회하는 메서드
     public TaskApiDto readTask(Long teamId, Long taskId, Long userId) {
         // 사용자가 해당 팀의 멤버인지 확인
-        isMemberOfTeam(userId, teamId);
+        taskValidationUtils.isMemberOfTeam(userId, teamId);
         //조직이 존재하는지 확인
-        getTeamById(teamId);
+        taskValidationUtils.getTeamById(teamId);
         //업무가 존재하는지 확인
         return taskApiRepository.findById(taskId)
                 .filter(taskApiEntity -> taskApiEntity.getTeam().getId().equals(teamId))
@@ -136,11 +102,12 @@ public class TaskApiService {
                 .orElseThrow(() -> new TodoAppException(ErrorCode.NOT_FOUND_TASK));
     }
 
+    //업무 전부 조회하는 메서드
     public List<TaskApiDto> readTasksAll(Long userId, Long teamId) {
         // 사용자가 해당 팀의 멤버인지 확인
-        isMemberOfTeam(userId, teamId);
+        taskValidationUtils.isMemberOfTeam(userId, teamId);
         //조직이 존재하는지 확인
-        getTeamById(teamId);
+        taskValidationUtils.getTeamById(teamId);
 
         List<TaskApiEntity> taskApiEntities = taskApiRepository.findAllByTeamId(teamId);
         return taskApiEntities.stream()
@@ -148,7 +115,7 @@ public class TaskApiService {
                 .collect(Collectors.toList());
     }
 
-    // 내 전체 업무 조회
+    // 내가 속한 팀들의 업무를 전부 조회하는 메서드
     public Map<TeamOverviewDto, List<TaskApiDto>> getMyTasks(Long userId) {
         Map<TeamOverviewDto, List<TaskApiDto>> myTasks = new HashMap<>();
         //by 최강성:TeamOverviewDto 추가
@@ -159,7 +126,7 @@ public class TaskApiService {
             List<TaskApiDto> taskApiDtoList = new ArrayList<>();
             for (TaskApiEntity taskApiEntity : taskApiEntityList)
                 //by 최강성 : 완료 된 업무는 제외
-                if (!taskApiEntity.getStatus().equals("완료")){
+                if (!taskApiEntity.getStatus().equals("완료")) {
                     taskApiDtoList.add(TaskApiDto.fromEntity(taskApiEntity));
                 }
             myTasks.put(TeamOverviewDto.fromEntity(teamEntity), taskApiDtoList);
@@ -167,59 +134,63 @@ public class TaskApiService {
         return myTasks;
     }
 
-    //업무 수정
+    //업무 수정 메서드
     public ResponseDto updateTask(Long userId, Long teamId, Long taskId, TaskApiDto taskApiDto) {
-        TeamEntity teamEntity = getTeamById(teamId);
+        //팀 존재 확인
+        TeamEntity teamEntity = taskValidationUtils.getTeamById(teamId);
         //기능을 사용할 수 있는지 확인
-        isAvailableFunction(teamEntity);
-        TaskApiEntity taskApiEntity = getTaskById(taskId);
+        taskValidationUtils.isAvailableFunction(teamEntity);
+        TaskApiEntity taskApiEntity = taskValidationUtils.getTaskById(taskId);
         //업무관리자와 대상업무가 맞는지 확인
-        validateTaskAndUser(userId, teamId, taskApiEntity);
+        taskValidationUtils.validateTaskAndUser(userId, teamId, taskApiEntity);
 
-        // 맞다면 진행
+        // 이전 상태 저장
+        String previousStatus = taskApiEntity.getStatus();
+
+        // 새로운 정보로 업무 업데이트
         updateTaskDetails(taskApiEntity, userId, taskApiDto);
+
+        //업무 수정후 업무 상태가 이전 상태와 달라졌을때만 알림보내기
+        if (!previousStatus.equals(taskApiEntity.getStatus())) {
+            sendTaskStatusNotification(taskApiEntity);
+        }
+
+        // 업무 저장
         taskApiRepository.save(taskApiEntity);
 
         return new ResponseDto("업무가 수정되었습니다.");
     }
-
+    //업무 업데이트 메서드
     private void updateTaskDetails(TaskApiEntity taskApiEntity, Long userId, TaskApiDto taskApiDto) {
-
-        // 이전 상태 저장
-        String previousStatus = taskApiEntity.getStatus();
         taskApiEntity.setUserId(userId);
         taskApiEntity.setTaskName(taskApiDto.getTaskName());
         taskApiEntity.setTaskDesc(taskApiDto.getTaskDesc());
         taskApiEntity.setStartDate(taskApiDto.getStartDate());
         taskApiEntity.setDueDate(taskApiDto.getDueDate());
 
-        //현재 날짜 추가
+        // 현재 날짜 추가
         LocalDate currentDate = LocalDate.now();
         taskApiEntity.setStatus("진행중");
 
-        //현재날짜가 아직 startDate 이전이면 진행예정
+        // 현재 날짜가 아직 startDate 이전이면 진행예정
         if (taskApiDto.getStartDate().isAfter(currentDate)) {
             taskApiEntity.setStatus("진행예정");
-        } // 현재날짜가 dueDate를 지났으면 완료
-        else if (taskApiDto.getDueDate().isBefore(currentDate)) {
+        } else if (taskApiDto.getDueDate().isBefore(currentDate)) {
+            // 현재 날짜가 dueDate를 지났으면 완료
             taskApiEntity.setStatus("완료");
-        }
-        //업무 수정후 업무 상태가 이전 상태와 달라졌을때만 알림보내기
-        if (!previousStatus.equals(taskApiEntity.getStatus())) {
-            sendTaskStatusNotification(taskApiEntity);
         }
     }
 
     //업무 삭제
     public ResponseDto deleteTask(Long userId, Long teamId, Long taskId) {
-        TeamEntity teamEntity = getTeamById(teamId);
+        TeamEntity teamEntity = taskValidationUtils.getTeamById(teamId);
 
         // 기능을 사용할 수 있는지 확인
-        isAvailableFunction(teamEntity);
+        taskValidationUtils.isAvailableFunction(teamEntity);
         // 업무 존재 확인
-        TaskApiEntity taskApiEntity = getTaskById(taskId);
+        TaskApiEntity taskApiEntity = taskValidationUtils.getTaskById(taskId);
         //업무관리자와 대상업무가 맞는지 확인
-        validateTaskAndUser(userId, teamId, taskApiEntity);
+        taskValidationUtils.validateTaskAndUser(userId, teamId, taskApiEntity);
 
         // 맞다면 진행
         taskApiRepository.deleteById(taskApiEntity.getId());
@@ -227,13 +198,13 @@ public class TaskApiService {
     }
 
 
-    // by 최강성: 팀내 내 업무 조회
+    // by 최강성: 팀내 내 업무 조회하는 메서드
     public List<TaskApiDto> getMyTasksInATeam(Long userId, Long teamId) {
         List<TaskApiDto> myTasksInATeam = new ArrayList<>();
         //팀이 존재하는지 확인
-        getTeamById(teamId);
+        taskValidationUtils.getTeamById(teamId);
         // 사용자가 해당 팀의 멤버인지 확인
-        isMemberOfTeam(userId, teamId);
+        taskValidationUtils.isMemberOfTeam(userId, teamId);
 
         List<TaskApiEntity> taskApiEntityList = taskApiRepository.findAllByTeamIdAndMember_UserId(teamId, userId);
         for (TaskApiEntity taskApiEntity : taskApiEntityList)
@@ -243,7 +214,7 @@ public class TaskApiService {
         return myTasksInATeam;
 
     }
-
+    //매일 정각에 업무를 자동 업데이트 하는 메서드
     @Scheduled(cron = "0 0 0 * * *")
     public void updateTaskStatusAuto() {
         LocalDate currentDate = LocalDate.now();
@@ -253,7 +224,7 @@ public class TaskApiService {
             updateTaskStatus(task, currentDate);
         }
     }
-
+    //업무 status를 수정하는 메서드
     private void updateTaskStatus(TaskApiEntity task, LocalDate currentDate) {
         if ("진행중".equals(task.getStatus())) {
             if (task.getDueDate().isBefore(currentDate)) {
@@ -271,7 +242,7 @@ public class TaskApiService {
             }
         }
     }
-
+    //업무 수정 시 전체 알림을 보내는 메서드
     private void sendTaskStatusNotification(TaskApiEntity taskApiEntity) {
         String formattedTime = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm"));
 
@@ -281,6 +252,7 @@ public class TaskApiService {
                 taskApiEntity.getStatus(), formattedTime));
         notifyTeamMembers(taskApiEntity.getTeam().getId(), notificationDto);
     }
+    // 팀에 속한 멤버들에게 알림을 보내는 메서드
     private void notifyTeamMembers(Long teamId, NotificationDto notificationDto) {
         // 팀에 속한 모든 멤버를 검색
         List<MemberEntity> teamMembers = memberRepository.findAllByTeamId(teamId);
@@ -289,17 +261,6 @@ public class TaskApiService {
         for (MemberEntity member : teamMembers) {
             Long userId = member.getUser().getId();
             notificationService.notify(userId, notificationDto);
-        }
-    }
-    //현재날짜에 맞춰서 업무 상태를 저장하는 메소드
-    private void setTaskStatus(TaskApiEntity taskApiEntity, LocalDate startDate, LocalDate dueDate) {
-        LocalDate currentDate = LocalDate.now();
-        taskApiEntity.setStatus("진행중");
-
-        if (startDate.isAfter(currentDate)) {
-            taskApiEntity.setStatus("진행예정");
-        } else if (dueDate.isBefore(currentDate)) {
-            taskApiEntity.setStatus("완료");
         }
     }
 }
--- src/main/java/com/example/todo/service/task/TaskValidationUtils.java
+++ src/main/java/com/example/todo/service/task/TaskValidationUtils.java
@@ -1,2 +1,72 @@
-package com.example.todo.service.task;public class TaskValidationService {
+package com.example.todo.service.task;
+
+import com.example.todo.domain.entity.TaskApiEntity;
+import com.example.todo.domain.entity.TeamEntity;
+import com.example.todo.domain.entity.UsersSubscriptionEntity;
+import com.example.todo.domain.entity.enums.SubscriptionStatus;
+import com.example.todo.domain.entity.user.User;
+import com.example.todo.domain.repository.TaskApiRepository;
+import com.example.todo.domain.repository.TeamReposiotry;
+import com.example.todo.domain.repository.UsersSubscriptionRepository;
+import com.example.todo.domain.repository.user.UserRepository;
+import com.example.todo.exception.ErrorCode;
+import com.example.todo.exception.TodoAppException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import static com.example.todo.service.team.TeamService.FREE_TEAM_PARTICIPANT_NUM;
+@Component
+@RequiredArgsConstructor
+public class TaskValidationUtils {
+    private final TaskApiRepository taskApiRepository;
+    private final TeamReposiotry teamRepository;
+    private final UserRepository userRepository;
+    private final UsersSubscriptionRepository usersSubscriptionRepository;
+
+    //회원인지 확인
+    public User getUserById(Long userId) {
+        return userRepository.findById(userId)
+                .orElseThrow(() -> new TodoAppException(ErrorCode.NOT_FOUND_USER));
+    }
+
+    //조직이 존재하는지 확인하는 메서드
+    public TeamEntity getTeamById(Long teamId) {
+        return teamRepository.findById(teamId)
+                .orElseThrow(() -> new TodoAppException(ErrorCode.NOT_FOUND_TEAM));
+    }
+
+    //업무 존재하는지 확인하는 메서드
+    public TaskApiEntity getTaskById(Long taskId) {
+        return taskApiRepository.findById(taskId)
+                .orElseThrow(() -> new TodoAppException(ErrorCode.NOT_FOUND_TASK));
+    }
+
+    //멤버인지 확인하는 메서드
+    public void isMemberOfTeam(Long userId, Long teamId) {
+        TeamEntity teamEntity = getTeamById(teamId);
+        if (teamEntity.getMembers().stream().noneMatch(member -> member.getUser().getId().equals(userId))) {
+            throw new TodoAppException(ErrorCode.NOT_MATCH_MEMBERID);
+        }
+    }
+    //by 안채연 : 기능을 사용할 수 있는지 확인하는 메서드
+    public void isAvailableFunction(TeamEntity teamEntity) {
+        if (teamEntity.getParticipantNumMax() > FREE_TEAM_PARTICIPANT_NUM) {
+            UsersSubscriptionEntity usersSubscription = usersSubscriptionRepository.findByUsersAndSubscriptionStatus(teamEntity.getManager(), SubscriptionStatus.ACTIVE)
+                    .orElseThrow(() -> new TodoAppException(ErrorCode.NOT_AVAILABLE_FUNCTION));
+            if (usersSubscription.getSubscription().getMaxMember() < teamEntity.getParticipantNumMax())
+                throw new TodoAppException(ErrorCode.NOT_AVAILABLE_FUNCTION);
+        }
+    }
+    //삭제와 수정을 하기위해 사용하는 메서드
+    public void validateTaskAndUser(Long userId, Long teamId, TaskApiEntity taskApiEntity) {
+        // 대상 업무가 대상 팀의 업무가 맞는지
+        if (!teamId.equals(taskApiEntity.getTeam().getId())) {
+            throw new TodoAppException(ErrorCode.NOT_MATCH_TEAM_AND_TASK);
+        }
+
+        // 팀 관리자 or 업무 담당자
+        if (!taskApiEntity.getUserId().equals(userId) && !taskApiEntity.getWorkerId().equals(userId)) {
+            throw new TodoAppException(ErrorCode.NOT_MATCH_USERID);
+        }
+    }
 }
